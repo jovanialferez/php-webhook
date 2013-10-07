0bc673e36907d9c3e20bcc0f69e73f6668de2b08
Jovani Alferez <vojalf@gmail.com>
2013-10-08 00:53:05 +0800

    Added execution of migration and cleaning up of assets.

--- hook.php
+++ hook.php
@@ -1,5 +1,8 @@
 <?php
 
+$yiiPath = 'path_to_yii_framework';
+$assetsPath = 'path_to_assets_folder';
+
 require_once('inc.php');
 header('Content-Type: text/plain');
 if (file_exists('key.php'))
@@ -7,13 +10,19 @@ if (file_exists('key.php'))
     $key = require('key.php');
     if ($key['current'] === $_SERVER['QUERY_STRING'])
     {
-        $submodule = getcwd();
-        $project = $submodule.'/../../../';
-        chdir(dirname($project));
+        // Let's assume this script is on root directory of the project
         _exec('pwd');
         _exec('git pull');
         _exec('git submodule update');
+
+        // Run migrations
+        _exec($yiiPath . '/yiic migrate --interactive=0');
+
+        // Clean up assets
+        _exec('/bin/rm -rf ' . $assetsPath);
+        mkdir($assetsPath);
     }
 }
 
 _log(var_export($_POST, true));
+
